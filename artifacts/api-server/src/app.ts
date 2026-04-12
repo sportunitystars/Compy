78283af0a6042bd97a872b06a4b5908ea96b7ec3
sportunitystars <luisgomezm10@gmail.com>
2026-04-12 20:22:49 +0200

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -12,14 +12,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req: IncomingMessage & { id?: string | number }) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res: ServerResponse) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
